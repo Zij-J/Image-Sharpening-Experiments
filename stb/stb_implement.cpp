04eb1b901b17b377663166d8c6a6e920b3813e84
Zij-J <zij.j@mail.com>
2025-09-28 21:39:50 +0800

    Adjust for github

--- stb/stb_implement.cpp
+++ stb/stb_implement.cpp
@@ -6,8 +6,10 @@
 //  to fix `warning C4996: 'sprintf': This function or variable may be unsafe.`
 
 
-// only bmp needed (.tif not supported), reduce binary size
+// only bmp, jpeg, png needed (.tif not supported), reduce binary size
 #define STBI_ONLY_BMP
+#define STBI_ONLY_JPEG
+#define STBI_ONLY_PNG
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
